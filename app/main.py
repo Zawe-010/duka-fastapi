c090961ea9410895a3921e7b8158d3c409e45353
Ailsa Don <ailsazawadi415@gmail.com>
2025-12-18 14:43:19 +0300

    Added codes

--- app/main.py
+++ app/main.py
@@ -7,9 +7,9 @@ from datetime import datetime
 import json
 
 from app.models import Product, Sale, User, Payment, session
-from auth.auth_service import get_current_user
-from auth.auth_routes import router as auth_router
-from mpesa import send_stk_push  
+from app.auth.auth_service import get_current_user
+from app.auth.auth_routes import router as auth_router
+from app.mpesa import send_stk_push  
 
 from pydantic import BaseModel
 
